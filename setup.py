4364b952701b6ce3883094676ea9d8d93757cbde
Teddy Wing <git@teddywing.com>
2014-04-20 00:22:05 -0400

    Add bare setup.py
    Just the essentials so that I can install my package. Will fill in the
    rest of the information later.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-sneak-peek',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='',
+    license='MIT',
+    author='Teddy Wing',
+    author_email='',
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[],
+)
