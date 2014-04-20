ba8146d30296d32b9720b274e75d0724c3559909
Teddy Wing <git@teddywing.com>
2014-04-20 12:26:21 -0400

    Enable Django admin in example project

--- examples/djangoproject/djangoproject/settings.py
+++ examples/djangoproject/djangoproject/settings.py
@@ -115,7 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'sneak_peek_tag',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
