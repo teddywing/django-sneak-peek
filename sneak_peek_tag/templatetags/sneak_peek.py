75fa16196e8b304d4c35581850bba8311f5dd1ed
Teddy Wing <git@teddywing.com>
2014-04-20 00:20:55 -0400

    Add sneak_peek app
    Includes:
    * Template tag
    * Static resources

---
+++ sneak_peek_tag/templatetags/sneak_peek.py
@@ -0,0 +1,36 @@
+from django.template import Library, Node
+
+register = Library()
+
+@register.tag(name="sneak_peek")
+def sneak_peek(parser, token):
+    tag = None
+    style = 'default'
+    try:
+        tag, style = token.split_contents()
+    except ValueError:
+        pass
+    nodelist = parser.parse(('endsneak_peek',))
+    parser.delete_first_token()
+    return SneakPeekWrapper(nodelist, style)
+
+
+class SneakPeekWrapper(Node):
+    def __init__(self, nodelist, style):
+        self.nodelist = nodelist
+        self.style = style
+
+    def render(self, context):
+        user = context['request'].user
+        
+        # TODO: update with permission check on user
+        user_is_blessed = True
+
+        if user_is_blessed:
+            content = self.nodelist.render(context)
+            wrapped_hidden_feature = """<div class="sneak-peek %s">
+                	%s
+                </div>""" % (self.style, content)
+            return wrapped_hidden_feature
+        else:
+            return ''
