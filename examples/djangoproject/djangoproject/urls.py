a0b983454e035413e7c37adfa561fb4269535571
Teddy Wing <git@teddywing.com>
2014-04-21 15:56:36 -0400

    Add page to example project for screenshots
    Need a page to use for screenshots for the README. Decided to
    integrate it into the example project.
    
    * Create screenshots.html template
    * Add route in urls.py

--- examples/djangoproject/djangoproject/urls.py
+++ examples/djangoproject/djangoproject/urls.py
@@ -7,5 +7,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^screenshots/$', TemplateView.as_view(template_name='screenshots.html'), name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
