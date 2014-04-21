a47efefde6b16c559d6fd2f8405f15337ecc73fe
Teddy Wing <git@teddywing.com>
2014-04-21 18:22:44 -0400

    Update setup.py
    Fill in more information. Readying for distribution.

--- setup.py
+++ setup.py
@@ -3,22 +3,39 @@ import os
 from setuptools import setup, find_packages
 
 
+# https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+
 setup(
     name='django-sneak-peek',
     version='0.0.1',
-    description='',
-    long_description='',
+    description='Django template tag to hide pre-release features in a ' \
+        'template',
+    long_description=(
+        read('README.md') + '\n\n' +
+        read('CHANGELOG')),
     url='',
     license='MIT',
     author='Teddy Wing',
-    author_email='',
+    author_email='pypi@teddywing.com',
     include_package_data=True,
     packages=find_packages(),
     install_requires=[
-        'Django >= 1.4'
+        'Django >= 1.4',
+        'South'
     ],
-    extras_require={
-        'South': ['South']
-    },
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2'
+    ],
 )
